e2477ddd1d2deff1bae9ffbd0d279807ad38ee5c
Matthew Collins <matthew.collins1991@yahoo.com>
2018-12-20 16:10:35 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,6 +1,7 @@
 # Add  code here!
 
 def prime?(num)
-  return if num <= 1
-  (2..Math.sqrt(num)).none? { |i| (num % i).zero? }
+  return false if num <= 1
+  Math.sqrt(num).to_i.downto(2).each {|i| return false if num % i == 0}
+  true
 end
